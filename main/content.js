d5f8f12ec1aa21e80a7f365b30976ba5782b3cba
aslkdfj <frog.beepers.0n@icloud.com>
2022-08-13 19:46:17 +0900

    update

--- main/content.js
+++ main/content.js
@@ -1,42 +1,48 @@
 const autoHighBilibili = () => {
-    let time1 = setInterval(()=> {
-        try {
-            let videoDom = document.querySelector("#live-player");
-            videoDom.dispatchEvent(new Event("mousemove"));
-            let quality = document.querySelector(".quality-wrap");
-            quality.dispatchEvent(new Event("mouseenter"));
-            if (
-                document.querySelector(".quality-it.selected").innerText !=
-                "原画"
-            ) {
-                let list = document.querySelectorAll(".quality-it");
-                list[0].click();
-                quality.dispatchEvent(new Event("mouseleave"));
-            }
-            clearInterval(time1);
-        } catch {}
-    }, 1000);
+	let time1 = setInterval(() => {
+		try {
+			let videoDom = document.querySelector("#live-player");
+			videoDom.dispatchEvent(new Event("mousemove"));
+			let quality = document.querySelector(".quality-wrap");
+			quality.dispatchEvent(new Event("mouseenter"));
+			if (
+				document.querySelector(".quality-it.selected").innerText !=
+				"原画"
+			) {
+				let list = document.querySelectorAll(".quality-it");
+				list[0].click();
+				quality.dispatchEvent(new Event("mouseleave"));
+			}
+			clearInterval(time1);
+		} catch {}
+	}, 1000);
 };
 
 const autoHighHuya = () => {
-    let time1 = setInterval(()=> {
-        try {
-            let videoDom = document.querySelector("#player-video");
-            videoDom.dispatchEvent(new Event("mousemove"));
-            let quality = document.querySelector(".player-videotype-cur");
-            quality.dispatchEvent(new Event("mousemove"));
-            if (
-                document.querySelector(".player-videotype-cur").innerText !=
-                "蓝光8M"
-            ) {
-                let list = document.querySelector(".player-videotype-list");
-                list.firstChild.click();
-                quality.dispatchEvent(new Event("mouseleave"));
-            }
-            clearInterval(time1);
-        } catch {}
-    }, 1000);
+	let time1 = setInterval(() => {
+		try {
+			let videoDom = document.querySelector("#player-video");
+			videoDom.dispatchEvent(new Event("mousemove"));
+			let quality = document.querySelector(".player-videotype-cur");
+			quality.dispatchEvent(new Event("mousemove"));
+			if (
+				document
+					.querySelector(".player-videotype-cur")
+					.innerText.includes("超清")
+			) {
+				let list = document.querySelector(".player-videotype-list");
+				list.firstChild.click();
+				quality.dispatchEvent(new Event("mouseleave"));
+			}
+			clearInterval(time1);
+		} catch {}
+	}, 1000);
 };
 
-autoHighBilibili();
-autoHighHuya();
+currentUrl = window.location.href;
+
+if (currentUrl.includes("live.bilibili")) {
+	autoHighBilibili();
+} else if (currentUrl.includes("huya")) {
+	autoHighHuya();
+}
